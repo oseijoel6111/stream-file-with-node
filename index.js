1c209bb7c43fc796808faa586617416b231fe3f9
oseijoel6111 <oseijoel6111@gmail.com>
2023-11-27 11:29:59 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    const file = fs.readFileSync("./index.html");
+    res.write(file);
+    res.end();
+  } else if (req.url == "/send-data") {
+    const userFile = req.headers["file-name"];
+    console.log(userFile);
+    req.on("data", (chunk) => {
+      fs.appendFileSync('userFile', chunk);
+    });
+  }
+});
+
+server.listen(2000, () => {
+  console.log(`Server started on http://localhost:${2000}`);
+});
